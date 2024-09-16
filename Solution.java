3ed71c3643ff82944e8fe7bc7a36bab43ebc1449
Madhan-2711 <160774259+Madhan-2711@users.noreply.github.com>
2024-09-16 18:32:00 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,24 +1,38 @@
 class Solution {
     int num  ;    
     public int myAtoi(String s) {
-        int regex = "[123456789]"
-        int sign = "-"
+        String sign = "-";
         if (s.contains("-")){
             sign = "-";
-        }esle{
+        }else{
             sign = "+";
         }
 
         for (int i  =0; i<s.length(); i++){
-            if(s.charAt(i).equals(0)){
+            if(s.charAt(i)  ==   '\\s'){
                 continue;
             }
-            if(s.charAt(i).contains(regex)){
-                num 
-                num = int
+           
+            if(Character.isDigit(s.charAt(i))){
+                 if(s.charAt(i)== '0'){
+                    continue;
+                 }
+                  num *= 10;
+                num += (int)s.charAt(i);
+            }else{
+                if(num>0){
+                    break;
+                }
+                return 0;
             }
+               
+            }
+              if(sign.equals("-")){
+            return (-1*num);
         }
+        return num;
+        }
+      
 
 
-    }
 }
